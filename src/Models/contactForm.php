d8d2c5c72e13fa51531f9f3d1c1c9fb692adc106
mansoor <mansoor@glowlogix.com>
2019-08-05 16:05:36 +0500

    initial commit

---
+++ src/Models/contactForm.php
@@ -0,0 +1,9 @@
+<?php
+    // MyVendor\Contactform\src\Models\ContactForm.php
+    namespace glowlogix\Contactform\Models;
+    use Illuminate\Database\Eloquent\Model;
+    class ContactForm extends Model
+    {
+        protected $guarded = [];
+        protected $table = 'contact';
+    }
